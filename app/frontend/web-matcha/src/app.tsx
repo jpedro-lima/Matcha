d28a9d8c522cff39ec7d46476519875e5a9d0538
João Pedro <jpedrones@hotmail.com>
2025-06-05 20:02:18 -0300

    Add sonner and tooltip components

--- app/frontend/web-matcha/src/app.tsx
+++ app/frontend/web-matcha/src/app.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter } from 'react-router'
 import './global.css'
 import { Router } from './router'
 import { ThemeProvider } from './components/theme/theme-provider'
+import { Toaster } from 'sonner'
 
 function App() {
 	return (
 		<ThemeProvider defaultTheme="dark" storageKey="matcha-theme">
+			<Toaster richColors />
 			<BrowserRouter>
 				<Router />
 			</BrowserRouter>
