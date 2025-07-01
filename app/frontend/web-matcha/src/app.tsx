6c71c7b1b590ef993501263706fcc15bf09791dc
João Pedro <jpedrones@hotmail.com>
2025-07-01 13:25:24 -0300

    Add QueryProvider to app

--- app/frontend/web-matcha/src/app.tsx
+++ app/frontend/web-matcha/src/app.tsx
@@ -3,14 +3,18 @@ import './global.css'
 import { Router } from './router'
 import { ThemeProvider } from './components/theme/theme-provider'
 import { Toaster } from 'sonner'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 function App() {
 	return (
 		<ThemeProvider defaultTheme="system" storageKey="matcha-theme">
 			<Toaster richColors />
-			<BrowserRouter>
-				<Router />
-			</BrowserRouter>
+			<QueryClientProvider client={queryClient}>
+				<BrowserRouter>
+					<Router />
+				</BrowserRouter>
+			</QueryClientProvider>
 		</ThemeProvider>
 	)
 }
