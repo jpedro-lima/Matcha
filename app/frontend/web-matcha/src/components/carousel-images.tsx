13d746ec0af2e315b717cac971a5675d730c5f6d
João Pedro <jpedro-lima@outlook.com>
2025-09-02 20:55:54 -0300

    fix(carousel): size card

--- app/frontend/web-matcha/src/components/carousel-images.tsx
+++ app/frontend/web-matcha/src/components/carousel-images.tsx
@@ -19,11 +19,11 @@ export function CarouselImages({ images }: CarouselImagesType) {
 		<Carousel className="m-5 sm:ml-20">
 			<CarouselPrevious className="hidden sm:flex" />
 
-			<CarouselContent className="h-[445px] cursor-grab active:cursor-grabbing sm:h-[700px]">
+			<CarouselContent className="h-[75vh] cursor-grab active:cursor-grabbing">
 				{images.map(({ url, size }) => {
 					return (
 						<CarouselItem key={url}>
-							<Card className="bg-muted m-0 flex h-full items-center justify-center p-0">
+							<Card className="bg-muted m-0 flex h-full items-center justify-center overflow-hidden p-0">
 								<CardContent>
 									<img src={url} className={`rounded-md ${size} `} />
 								</CardContent>
